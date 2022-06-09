37beb2880d65cbb68ca47a56e257cb20f60fb73f
Jese <jeseravi@gmail.com>
2022-06-09 18:05:41 -0400

    Books can be added now. Duplication error added.

--- javascript.js
+++ javascript.js
@@ -6,37 +6,75 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
-//need to call functions before the closeForm() is called
-//or else data will be deleted
-var myForm=document.getElementById("myForm");
-myForm.onsubmit=function(){
+//array of each book
+let myLibrary=[];
 
-       
-}
 
-//stores form data in local storage
+//puts data from form into array book
 function saveData(){
-    let title,author;
-    let readStatus = document.querySelector('input[name = "readStatus"]:checked').value;
-    title=document.getElementById("title").value;
-    author=document.getElementById("author").value;
-    localStorage.setItem("title",title);
-    localStorage.setItem("author",author);
-    localStorage.setItem("readStatus",readStatus);
 
-    createCard();  
+    let book;
 
-}
-
-//take data from local storage and make a card with it    
-function createCard(){
-
-    let testDiv = document.createElement("div");
-    testDiv.innerHTML="Testing"
-    document.getElementById("libraryArea").appendChild(testDiv);
-
-}
     
+    //terminates function and throws error if book title and author has already been added
+    //if(book[0]&&book[1]){
 
+   // }
+
+    let readStatus = document.querySelector('input[name = "readStatus"]:checked').value;
+    let title=document.getElementById("title").value;
+    let author=document.getElementById("author").value;
+
+    book=[title,author,readStatus];
+
+    for(i=0;i<myLibrary.length;i++){
+        if(myLibrary[i]=book[0]){
+            alert("A book with this title has already been added to your collection");
+
+            document.getElementById('form').reset();
+
+            return
+        }
+
+    }
+
+    myLibrary.push(book);
+
+    //reset the form
+    document.getElementById('form').reset();
+
+//take data from the book array and make a card with it    
+
+    generateElement("div",book[0],"","libraryArea",book[0],"form-container generatedFormContainer")
+    generateElement("div","label","Title: " + book[0],book[0],"generatedTitle","generatedLabel")
+    generateElement("div","label","Author: " + book[1],book[0],"generatedDiv","generatedLabel")
+    generateElement("hr","label","",book[0],"generatedHr","generatedLabel")
+    generateElement("div","label","Read: " + book[2],book[0],"generatedDiv","generatedLabel")
+    generateElement("button","label","Update",book[0],"updateButton","btn")
+    generateElement("button","label","Close",book[0],"updateButton","btn cancel")
+
+
+
+
+
+
+}
+
+
+function generateElement(whatIsIt,entryName,innerHTML,whereToPut,id,chosenClass){
+
+    let generateWhat = whatIsIt;
+    let generateEntry = entryName;
+    let generateWhere = whereToPut;
+    let generateHTML = innerHTML;
+    let generateId=id;
+    let generateClass=chosenClass;
+
+    generateEntry = document.createElement(generateWhat);
+    generateEntry.setAttribute("id",generateId);
+    generateEntry.setAttribute("class",generateClass);
+    generateEntry.innerHTML=generateHTML;
+    document.getElementById(generateWhere).appendChild(generateEntry);
+}
 
 
