ae0f83a54151e93a59d3bf7e4f75e57d043a16e8
Jese <jeseravi@gmail.com>
2022-06-09 09:54:11 -0400

    Add function to process local data

--- javascript.js
+++ javascript.js
@@ -6,37 +6,37 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
+//need to call functions before the closeForm() is called
+//or else data will be deleted
 var myForm=document.getElementById("myForm");
 myForm.onsubmit=function(){
 
+       
 }
-
 
 //stores form data in local storage
-function saveData()
-    {
-        let title,author;
-        var readStatus = document.querySelector('input[name = "readStatus"]:checked').value;
-        console.log(readStatus);
-        title=document.getElementById("title").value;
-        author=document.getElementById("author").value;
-        localStorage.setItem("title",title);
-        localStorage.setItem("author",author);
-        localStorage.setItem("readStatus",readStatus);
-    }
+function saveData(){
+    let title,author;
+    let readStatus = document.querySelector('input[name = "readStatus"]:checked').value;
+    title=document.getElementById("title").value;
+    author=document.getElementById("author").value;
+    localStorage.setItem("title",title);
+    localStorage.setItem("author",author);
+    localStorage.setItem("readStatus",readStatus);
 
-function generateElement(whatIsIt,entryName,innerHTML,whereToPut,id,chosenClass){
+    createCard();  
 
-    let generateWhat = whatIsIt;
-    let generateEntry = entryName;
-    let generateWhere = whereToPut;
-    let generateHTML = innerHTML;
-    let generateId=id;
-    let generateClass=chosenClass;
-
-    generateEntry = document.createElement(generateWhat);
-    generateEntry.setAttribute("id",generateId);
-    generateEntry.setAttribute("class",generateClass);
-    generateEntry.innerHTML=generateHTML;
-    document.getElementById(generateWhere).appendChild(generateEntry);
 }
+
+//take data from local storage and make a card with it    
+function createCard(){
+
+    let testDiv = document.createElement("div");
+    testDiv.innerHTML="Testing"
+    document.getElementById("libraryArea").appendChild(testDiv);
+
+}
+    
+
+
+
