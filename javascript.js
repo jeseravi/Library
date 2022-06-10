1a792befa4bc6f1ee2612d8ce18083899e5fa864
Jese <jeseravi@gmail.com>
2022-06-10 09:07:38 -0400

    Add functionality to update and remove buttons.

--- javascript.js
+++ javascript.js
@@ -37,8 +37,10 @@ function saveData(){
     let readStatus = document.querySelector('input[name = "readStatus"]:checked').value;
     let title=document.getElementById("title").value;
     let author=document.getElementById("author").value;
+    //index helper helps find and remove this book later
+    let indexHelper = title+author;
 
-    book=[title,author,readStatus];
+    book=[title,author,readStatus,indexHelper];
 
     myLibrary.push(book);
 
@@ -49,13 +51,63 @@ function saveData(){
 
     generateElement("div",book[0],"","libraryArea",book[0]+book[1],"form-container generatedFormContainer")
     generateElement("div","label",book[0],book[0]+book[1],"generatedTitle","generatedLabel")
-    generateElement("div","label","Author: " + book[1],book[0]+book[1],"generatedDiv","generatedLabel")
+    generateElement("div","label","Author: " + book[1],book[0]+book[1],"generatedDiv","generatedDiv")
     generateElement("hr","label","",book[0]+book[1],"generatedHr","generatedLabel")
-    generateElement("div","label","Read: " + book[2],book[0]+book[1],"generatedDiv","generatedLabel")
-    generateElement("button","label","Update",book[0]+book[1],"updateButton","btn")
-    generateElement("button","label","Delete",book[0]+book[1],"updateButton","btn cancel")
+    generateElement("div","label","Read: " + book[2],book[0]+book[1],"generatedDiv"+book[0]+book[1],"generatedDiv")
+    generateElement("button","label","Update",book[0]+book[1],"updateButton"+book[0]+book[1],"btn")
+    generateElement("button","label","Delete",book[0]+book[1],"closeButton"+book[0]+book[1],"btn cancel")
 
-//make the close button
+//delete button event listener
+    let closeButtonListener = document.getElementById("closeButton"+book[0]+book[1]);
+    closeButtonListener.addEventListener("click",function(event){
+
+        document.getElementById(book[0]+book[1]).remove();
+
+        //check index in myLibrary. Remove the necessary book.
+
+        for(i=0;i<myLibrary.length;i++){
+
+            if(myLibrary[i][3]==indexHelper){
+                myLibrary = myLibrary.splice(i,i);
+            }
+
+            else{
+                console.log("not yet")
+            }
+
+        }
+
+    })
+
+//update button event listener
+//create the function that toggles a book's read status on your Book prototype instance
+let updateButtonListener = document.getElementById("updateButton"+book[0]+book[1]);
+updateButtonListener.addEventListener("click",function(event){
+
+
+
+    for(i=0;i<myLibrary.length;i++){
+        let testing=this.id.substring(12)
+        if(myLibrary[i][2]=="Yes" && myLibrary[i][3]==testing){
+            myLibrary[i][2]="No";
+
+            //change the displayed value to No
+            document.getElementById("generatedDiv"+book[0]+book[1]).innerHTML="Read: No"
+                    
+        }
+
+        else{
+            myLibrary[i][2]="Yes";
+            document.getElementById("generatedDiv"+book[0]+book[1]).innerHTML="Read: Yes"
+        }
+    }
+
+
+    //else(//if read//)
+
+    //update info on card based on this prototype change
+})
+
 
 
 }
